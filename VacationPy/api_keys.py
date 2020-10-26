01f67a31e43e6f349ac3fec33607a6334c3149a6
AustinRCole <AustinRCole2@gmail.com>
2020-10-26 13:00:45 -0400

    finished WeatherPy and started Vacation

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBySIhH_N4CEGDzSlNgxhOij7dIPVkwa-c"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "3811f48f0950217f49df9d647fd4a147"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
